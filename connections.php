c27e883e0179c798f4e4f61d1385138cf0e74efb
johannesclaus30 <clausja@students.nu-lipa.edu.ph>
2025-10-21 13:47:06 +0800

    connect

--- connections.php
+++ connections.php
@@ -1,7 +1,7 @@
 <?php
 
-#$connections = mysqli_connect("mysql.hostinger.com","u815942348_graphio_db","@Graphio12345","u815942348_graphio_db");
-$connections = mysqli_connect("localhost","root","","graphio_dbv3");
+$connections = mysqli_connect("mysql.hostinger.com","u815942348_graphio_db","@Graphio12345","u815942348_graphio_db");
+#$connections = mysqli_connect("localhost","root","","graphio_dbv3");
 
 if(mysqli_connect_errno()) {
     echo "Failed to connect to MySQL: " . mysqli_connect_error();
